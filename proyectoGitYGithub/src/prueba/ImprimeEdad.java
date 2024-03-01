7995b4a7d1965862fb046bcaccb15247fff8670d
Jesús <Jess@DESKTOP-V0OKO40>
2024-03-01 15:54:20 +0100

    Este es un ejemplo de comentario

---
+++ proyectoGitYGithub/src/prueba/ImprimeEdad.java
@@ -0,0 +1,15 @@
+package prueba;
+public class ImprimeEdad {
+	public static void main(String[] args) {
+
+		int edad=35; 
+		System.out.println (edad);
+
+		edad=75;
+		System.out.println (edad);
+
+	}
+
+}
+
+
